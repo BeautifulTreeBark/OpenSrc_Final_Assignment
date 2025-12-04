3c5441634f35458483a9a65cda9b398f28d8968a
BeautifulTreeBark <jhm224816@gmail.com>
2025-12-04 19:26:19 +0900

    committed by hyunmin

--- averageScore.c
+++ averageScore.c
@@ -1,16 +1,16 @@
 void averageScore(void) {
-    printf("평균 점수 계산기\n");
+    printf("average score\n");
 
     int how;
     double result = 0.0;
 
-    printf("점수 개수 : ");
+    printf("Number of data : ");
     scanf("%d", &how);
 
     double* data = (double*)calloc(how, sizeof(double));
 
     for (int i = 0; i < how; i++) {
-        printf("%d번쨰 점수 : ", i + 1);
+        printf("%d data : ", i + 1);
         scanf("%lf", &data[i]);
 
         result += data[i];
@@ -18,14 +18,14 @@ void averageScore(void) {
 
     result /= how;
 
-    printf("데이터 :\n");
+    printf("inputs :\n");
     printf("[");
     for (int i = 0; i < how; i++) {
         printf(" %.3f ", data[i]);
     }
     printf("]");
 
-    printf("\n평균 : %.3f", result);
+    printf("\naverage : %.3f", result);
 
     free(data);
 }
